529b1dd862a529fd7c743d8dcc292a7777fa5f66
Alaa Eddine <alaneddin91@gmail.com>
2021-01-29 13:00:06 +0100

    update

--- src/server.js
+++ src/server.js
@@ -29,7 +29,7 @@ server.use(notFoundHandler);
 server.use(genericErrorHandler);
 
 models.sequelize
-  .sync({ force: false})
+  .sync({ force: false })
 
   .then((result) => {
     server.listen(port || 3001, () => console.log("Running on port " + port));
